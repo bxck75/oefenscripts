5902834b3f50bf6024bd79446742d23a1981f48c
Boudewijn <goldenkooy@gmail.com>
2014-12-12 00:02:59 +0100

    cleaned up the code a bit

--- oef3/module/event/eventController.php
+++ oef3/module/event/eventController.php
@@ -3,51 +3,40 @@ class eventController extends Controller{
         // overview van events
 	public function overview(){
 		$this->setTitle('Overzicht events');
-		
 		$this->loadModel('event');
-		
 		$this->events = $this->model->getEvents();
-		
 		$this->render('event_overview.tpl');
 	}
         // 1 event bekijken
 	public function oneEvent(){
 		$this->setTitle('Een event bekijken');
-                
                 $this->id = $_GET['event_id'];
-                
                 $this->loadModel('event');
-                
-		$this->event = $this->model->getEvent($this->id);
-		
-		$this->render('event_viewone.tpl');
+                $this->event = $this->model->getEvent($this->id);
+                $this->render('event_viewone.tpl');
 	}
-        // event toevoegen
+        // event formulier afbeelden
 	public function addEvent(){
 		$this->setTitle('Voeg events toe');
-		
-		$this->render('event_add.tpl');
+                $this->render('event_add.tpl');
 	}
+        // event verwijderen
 	public function deleteEvent(){
-		$this->setTitle('delete event');
+		$this->setTitle('Delete event');
                 $this->id = $_GET['event_id'];
                 $this->loadModel('event');
-		$this->added_event = $this->model->deleteEvent($this->id);
-		
-		$this->render('event_delete.tpl');
+		$this->deleted_event = $this->model->deleteEvent($this->id);
+                $this->render('event_delete.tpl');
 	}
-        
+        //  event inserten in de database
 	public function insertEvent(){
 		$this->setTitle('Voeg event toe');
-                
                 $this->name = $_POST['name'];
                 $this->startdate = $_POST['start_date'];
                 $this->enddate = $_POST['end_date'];
                 $this->price = $_POST['price'];
-                
                 $this->loadModel('event');
 		$this->added_event = $this->model->insertEvent($this->name,$this->startdate,$this->enddate,$this->price);
-		
-		$this->render('event_added.tpl');
+                $this->render('event_added.tpl');
 	}
 }
